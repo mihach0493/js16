de23d6c3c69805d9d2d5d5c429ebee9ccdc88d5a
Mykhailo Chui <mihach0493@gmail>
2020-09-13 21:30:01 +0200

    js16

---
+++ main.js
@@ -0,0 +1,15 @@
+let number = 20;
+
+let array = [11, 9];
+
+let boolean = false
+
+if (number === 20) {
+    console.log('jest rowna 20');
+}
+else if (number === 11) {
+    console.log('jest rowna 11');
+}
+else {
+    console.log('Żaden z powyższych warunków NIE jest spełniony');
+}
